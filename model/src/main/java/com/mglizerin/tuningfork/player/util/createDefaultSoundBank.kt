886285ba67b1039370107876f486c6ce34c60d08
Mikhail <glizerin@gmail.com>
2019-06-14 22:25:37 +0300

    [TuningFork] Добавил набор сэмлов, доработал модель банка звуков

--- model/src/main/java/com/mglizerin/tuningfork/player/util/createDefaultSoundBank.kt
+++ model/src/main/java/com/mglizerin/tuningfork/player/util/createDefaultSoundBank.kt
@@ -9,10 +9,52 @@ import com.mglizerin.tuningfork.sound.types.*
 
 fun createDefaultSoundBank(): ISoundBank {
     val defaultSounds: Map<IPitch, Int> = mapOf(
-        Pitch(Note.D, Accidental.NATURAL, 4) to R.raw.d4,
-        Pitch(Note.F, Accidental.SHARP, 4) to R.raw.fis4,
-        Pitch(Note.A, Accidental.NATURAL, 4) to R.raw.a4,
-        Pitch(Note.D, Accidental.NATURAL, 5) to R.raw.d5
+        Pitch(Note.G, Accidental.NATURAL, 3) to R.raw.f_43,
+        Pitch(Note.A, Accidental.NATURAL, 3) to R.raw.f_45,
+        Pitch(Note.B, Accidental.NATURAL, 3) to R.raw.f_47,
+        Pitch(Note.C, Accidental.NATURAL, 4) to R.raw.f_48,
+        Pitch(Note.D, Accidental.NATURAL, 4) to R.raw.f_50,
+        Pitch(Note.E, Accidental.NATURAL, 4) to R.raw.f_52,
+        Pitch(Note.F, Accidental.NATURAL, 4) to R.raw.f_53,
+        Pitch(Note.G, Accidental.NATURAL, 4) to R.raw.f_55,
+        Pitch(Note.A, Accidental.NATURAL, 4) to R.raw.f_57,
+        Pitch(Note.B, Accidental.NATURAL, 4) to R.raw.f_59,
+        Pitch(Note.C, Accidental.NATURAL, 5) to R.raw.f_60,
+        Pitch(Note.D, Accidental.NATURAL, 5) to R.raw.f_62,
+        Pitch(Note.E, Accidental.NATURAL, 5) to R.raw.f_64,
+        Pitch(Note.F, Accidental.NATURAL, 5) to R.raw.f_65,
+        Pitch(Note.G, Accidental.NATURAL, 5) to R.raw.f_67,
+
+        Pitch(Note.G, Accidental.SHARP, 3) to R.raw.f_44,
+        Pitch(Note.A, Accidental.SHARP, 3) to R.raw.f_46,
+        Pitch(Note.B, Accidental.SHARP, 3) to R.raw.f_48,
+        Pitch(Note.C, Accidental.SHARP, 4) to R.raw.f_49,
+        Pitch(Note.D, Accidental.SHARP, 4) to R.raw.f_51,
+        Pitch(Note.E, Accidental.SHARP, 4) to R.raw.f_53,
+        Pitch(Note.F, Accidental.SHARP, 4) to R.raw.f_54,
+        Pitch(Note.G, Accidental.SHARP, 4) to R.raw.f_56,
+        Pitch(Note.A, Accidental.SHARP, 4) to R.raw.f_58,
+        Pitch(Note.B, Accidental.SHARP, 4) to R.raw.f_60,
+        Pitch(Note.C, Accidental.SHARP, 5) to R.raw.f_61,
+        Pitch(Note.D, Accidental.SHARP, 5) to R.raw.f_63,
+        Pitch(Note.E, Accidental.SHARP, 5) to R.raw.f_65,
+        Pitch(Note.F, Accidental.SHARP, 5) to R.raw.f_66,
+        Pitch(Note.G, Accidental.SHARP, 5) to R.raw.f_68,
+
+        Pitch(Note.A, Accidental.FLAT, 3) to R.raw.f_44,
+        Pitch(Note.B, Accidental.FLAT, 3) to R.raw.f_46,
+        Pitch(Note.C, Accidental.FLAT, 4) to R.raw.f_47,
+        Pitch(Note.D, Accidental.FLAT, 4) to R.raw.f_49,
+        Pitch(Note.E, Accidental.FLAT, 4) to R.raw.f_51,
+        Pitch(Note.F, Accidental.FLAT, 4) to R.raw.f_52,
+        Pitch(Note.G, Accidental.FLAT, 4) to R.raw.f_54,
+        Pitch(Note.A, Accidental.FLAT, 4) to R.raw.f_56,
+        Pitch(Note.B, Accidental.FLAT, 4) to R.raw.f_58,
+        Pitch(Note.D, Accidental.FLAT, 5) to R.raw.f_61,
+        Pitch(Note.E, Accidental.FLAT, 5) to R.raw.f_63,
+        Pitch(Note.F, Accidental.FLAT, 5) to R.raw.f_64,
+        Pitch(Note.G, Accidental.FLAT, 5) to R.raw.f_66,
+        Pitch(Note.A, Accidental.FLAT, 5) to R.raw.f_68
     )
 
     val soundBank = SoundBank()
--- model/src/main/java/com/mglizerin/tuningfork/samples/presets/GDur.kt
+++ model/src/main/java/com/mglizerin/tuningfork/samples/presets/GDur.kt
@@ -9,6 +9,9 @@ import com.mglizerin.tuningfork.sound.types.Accidental
 import com.mglizerin.tuningfork.sound.types.Mode
 import com.mglizerin.tuningfork.sound.types.Note
 
+val G4_D5_B4_G4_H = createChord(G5H, D5H, B4H, G4H)
+val F5S_D5_C5_A4_H = createChord(F5SH,D5H, C5H, A4H)
+
 fun createGDurSamples(tempo: ITempo): List<ISample> {
     val key = Key(Note.G, Accidental.NATURAL, Mode.MAJOR)
     val samples = mutableListOf<ISample>()
@@ -17,7 +20,8 @@ fun createGDurSamples(tempo: ITempo): List<ISample> {
         listOf(D5_Q, B4_Q, G4_H),
         listOf(G4_Q, B4_Q, D5_Q, B4_Q, G4_H),
         listOf(G4_Q, B4_Q, G4_Q, A4_Q, F4S_Q, D4_H),
-        listOf(C5_Q,A4_Q, F4S_Q, D4_H)
+        listOf(C5_Q, A4_Q, F4S_Q, D4_H),
+        listOf(G4_D5_B4_G4_H)
     )
 
     data.forEach { chord ->
